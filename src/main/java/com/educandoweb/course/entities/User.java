b583e9b9091080a1854dbbd36ffc7c52d2fabc0b
davi7709 <davialealves@gmail.com>
2024-05-06 17:31:15 -0300

    [200~H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -2,18 +2,27 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	public User() {
-		
+	public User() {	
 	}
 
 	public User(long id, String name, String email, String phone, String password) {
